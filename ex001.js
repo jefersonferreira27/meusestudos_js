6855444eb5d80d2dd0cc39a546f2df297e3408b2
Jeferson Ferreira <jefersonferreira27@outlook.com>
2023-05-14 18:10:25 -0300

    Primeiros exe

---
+++ ex001.js
@@ -0,0 +1,26 @@
+// Exercicios de lógica de programação
+
+// Exibir os números de 1 a 10 na tela 
+let i = 1
+for(i; i<= 10; ){
+    console.log(i)
+    i++
+}
+
+// Calcular a média de três notas e exibir a nota
+let nota1 = 6.5
+let nota2 = 8
+let nota3 = 9.25
+
+let media = (nota1 + nota2 + nota3)/ 3
+console.log(media);
+
+// Verificar se um número é par ou ímpar
+let num = 51; // Escolha um número
+if(num % 2 == 1){
+    console.log("Número ímpar: " + num)
+} else if(num % 2 == 0){
+    console.log("Número par: " + num)
+}
+
+// Verificar se o ano é bissexto
