9862259b8a0cec681ae573492577dd1ea41dadbe
Jeferson Ferreira <jefersonferreira27@outlook.com>
2023-05-14 18:49:07 -0300

    Atualizacao

--- ex001.js
+++ ex001.js
@@ -23,4 +23,30 @@ if(num % 2 == 1){
     console.log("Número par: " + num)
 }
 
+// Programa que recebe 3 números e retorna o maior
+function maiorNumero(num1, num2, num3){
+    if(num1>num2 && num1>num3){
+        return (num1 + " é o maior número!")
+    } else if(num2>num1 && num2>num3){
+        return (num2 + " é o maior número!")
+    } else if(num3>num1 && num3>num2){
+        return (num3 + " é o maior número!")
+    }
+}
+// Chamada da função
+console.log(maiorNumero(10,15,30));
+
+
 // Verificar se o ano é bissexto
+function anoBissexto(anoEscolhido){
+    if(anoEscolhido % 4 === 0 && anoEscolhido % 100 !==0 || anoEscolhido % 400 === 0){
+        return (anoEscolhido + " é um ano bissexto!")
+    } else {
+        return (anoEscolhido + " não é um ano bissexto!")
+    }
+}
+
+// Chamada da função
+console.log(anoBissexto(2023))
+console.log(anoBissexto(2024))
+
