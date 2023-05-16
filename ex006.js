f68560cc4477f5fe0fe68717e37d41e2ea257f45
Jeferson Ferreira <jefersonferreira27@outlook.com>
2023-05-15 23:31:25 -0300

    Exercicio 06

---
+++ ex006.js
@@ -0,0 +1,7 @@
+// Escreva um programa que receba um número como entrada e exiba todos os números primos menores ou iguais a ele.
+function numeroPrimo(number){
+    if(number % number === 0 && number % 1 === 0){
+        return number
+    }
+}
+
