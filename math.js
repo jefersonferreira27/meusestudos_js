8779af37f5fe11d3d20f1dff95e7e5eb2f2573e1
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-17 12:54:45 -0300

    Função math

---
+++ math.js
@@ -0,0 +1,15 @@
+// Função Math para exibir o maior número
+let maior = Math.max(6,12,28,55,8);
+console.log(maior);
+
+// Função Math para exibir o menor número
+let menor = Math.min(6,12,28,55,8);
+console.log(menor);
+
+// Função Math para arredondar 
+let arredondar = Math.round(6.87569);
+console.log(arredondar);
+
+// Função Math para arredondar pra cima
+let arredondarParaCima = Math.ceil(6.87569);
+console.log(arredondarParaCima);
