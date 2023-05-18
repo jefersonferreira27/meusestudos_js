6bd9cf364bd92b49c5ce17a40ec8663a53d4daf5
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-18 13:19:58 -0300

    Outro exercicio de array

--- loop_em_array.js
+++ loop_em_array.js
@@ -12,4 +12,15 @@ for(let i = 0; i < valores.length; i++){
   soma += valores[i];
 }
 
+// Exibindo resultado
 console.log(soma);
+
+// Exibir o menor nome em um array
+let nomes = ["Jeferson", "Edson", "Simone"];
+let menorNome = nomes[0];
+for(let i = 0; i < nomes.length; i++){
+  if(nomes[i]> menorNome){
+    menorNome = array[i];
+  }
+}
+
