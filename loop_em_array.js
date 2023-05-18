7ed3ce906821aec45ab2cbed200355070d3f7013
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-18 13:03:24 -0300

    Exercicio de array

--- loop_em_array.js
+++ loop_em_array.js
@@ -4,3 +4,10 @@ let times = ["Bahia", "Goiás", "Avai"];
 for(let i = 0; i <= times.length; i++){
   console.log(times[i]);
 }
+
+// Calcular todos os valores do array
+let = valores [25, 50, 25];
+for(let soma = 0; soma <= valores.length; i++){
+  soma += valores[soma];
+  console.log(soma);
+}
