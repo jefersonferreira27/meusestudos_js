18e6398f020b0243ae6062042e2bacfc126219f1
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-22 12:40:00 -0300

    Create w3schools01.js
    Estudo numero 01 de Javascript com W3schools

---
+++ w3schools01.js
@@ -0,0 +1,41 @@
+// Sintaxe  JAVASCRIPT 
+
+// Como criar variáveis
+var x;
+let y
+const z;
+
+// Como usar variáveis
+let total = x + y + z;
+
+// Valores Javascript
+  - Valores fixos(literais)
+  - Valores variáveis(variáveis)
+  
+// Operadores Javascript
+  + - * /
+  
+ // Operador de atribuição
+  =
+  let x,y;
+  x = 5;
+  y = 6;
+  
+ // Expressões em Javascript
+ Podem ser valores e variáveis
+ 5 * 10
+ x * 10
+ 
+ // Comentários em Javascript
+    // - Em linha
+    /* */ - Em bloco
+    
+  // Nomes de variáveis em Javascript (DEVEM COMEÇAR COM:)
+    - Uma letra
+    - Um cifrão
+    - Ou um sublinhado()
+   
+  // - Camel Case em JS - Case sensitive
+    O Javascript diferencia de maiuscula e minuscula
+    
+   
