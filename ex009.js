cdd1da57f00232f6156d3de0e4007548230d91bf
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-23 13:12:28 -0300

    Exercicio sobre função

---
+++ ex009.js
@@ -0,0 +1,17 @@
+// Função SOMA
+function soma (a,b){
+  return a + b;
+}
+
+// Resultado será 15
+console.log(soma(5,10));
+
+
+// Função TAMANHO da String; 
+function tamanhoString (string){
+  return string.length;
+}
+
+// Resultado será o comprimento da string 
+console.log(tamanhoString("Futebol")) // Nesse caso será 7
+
