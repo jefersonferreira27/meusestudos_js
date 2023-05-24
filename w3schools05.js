3641d658503ab61836d22248b1d3f15adf8b51f5
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-24 13:03:01 -0300

    Eventos em Javascript

---
+++ w3schools05.js
@@ -0,0 +1,13 @@
+// Eventos em JS
+// São coisas que acontecem nos elementos HTML
+// Pode ser a interação do navegador ou usuário
+
+// Tipos de eventos
+onchange - // Um elemento HTML foi alterado
+onclick - // O usuário clica em um elemento HTML
+onmouseover - // O usuário move o mouse sobre um elemento HTML
+onmouseout - // O usuário move o mouse para longe de um elemento HTML
+onkeydown - // O usuário pressiona um tecla do teclado
+onload - // O navegador terminou de carregar a página
+
+
