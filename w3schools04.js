22f1e3183a13c7babca5730712a38d910e5384fb
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-24 12:48:30 -0300

    Objetos em Javascript
    Conceito inicial em objetos

---
+++ w3schools04.js
@@ -0,0 +1,28 @@
+// Objetos em Javascript
+// Representação da vida real
+// Um objeto possui propriedades
+// Os valores são escritos como pares nome:valor(nome e valor separados por dois pontos)
+// É comum declarar um objeto com a PALAVRA-CHAVE CONST
+
+// Exemplo:
+const times = {
+  //Os paises são as propriedades e os times são os valores
+  espanha: "ATM",
+  italia: "Juve",
+  franca: "PSG",
+};
+
+// Acessar propriedades do objeto
+times.espanha()
+// Ou
+times["espanha"]
+
+// Métodos de objeto
+// Métodos são ações que podem ser executadas em objetos
+
+// O que é THIS em JS?
+// This refere-se a um objeto
+
+
+
+
