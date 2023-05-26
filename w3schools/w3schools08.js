58864635d41eeb37a21f9194afd3255b04b32a17
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-26 13:19:07 -0300

    Método de pesquisa de String

---
+++ w3schools/w3schools08.js
@@ -0,0 +1,23 @@
+// Métodos de Pesquisa de String
+
+// Método indexOf()
+// Retorna o índice(posição) da primeira ocorrência de uma string em uma string
+
+let text = "Estude Javascript todos os dias"
+let index = text.indexOf("Javascript"); // Resultado é: [7]
+
+//Método lastIndexOf()
+// Retorna o índice da última ocorrência de um texto especificado em uma string
+
+let text = "Estude Javascript todos os dias"
+let index = text.lastIndexOf("Javascript"); // Resultado é: [17]
+
+// Método search()
+// Retorna uma string por uma string (ou uma expressão regular) e retorna a posição da correspondencia
+
+let time = "O maior time da Espanha é o Barcelona"
+time.search("Barcelona"); // Resultado é: []
+
+// Método includes()
+// Retorna true se uma string contiver o valor especificado, caso contrário retorna false.
+
