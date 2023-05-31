3926a99b28dda4eb40b8d09a41497fbc005b71a8
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-05-31 13:06:06 -0300

    Estudo inicial de métodos matemáticos

---
+++ w3schools/w3schools16.js
@@ -0,0 +1,18 @@
+//Métodos matemáticos/Math
+
+//Sintaxe
+Math.propriedade
+
+//Transformar número para inteiro
+
+//Math.round
+//Retorna valor arredondado para o inteiro mais próximo
+
+//Math.ceil
+//Retorna valor arredondado para o número inteiro mais proximo
+
+//Math.floor
+//Retorna valor arredondado para baixo para o inteiro mais próximo
+
+//Math.trunc
+//Retorna a parte inteiro do valor
