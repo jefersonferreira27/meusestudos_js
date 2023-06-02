cc68f2f480a84a8839cf3fcd793d4d22ef3268a2
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-06-02 13:07:53 -0300

    Prévia sobre new set ()
    Em breve irei dar continuidade sobre esse conteúdo

---
+++ w3schools/w3schools19.js
@@ -0,0 +1,27 @@
+// Conjuntos em JS
+// É uma coleção de valores únicos 
+
+// Como criar um conjunto
+/* - Passando um Array para new set()
+   - Crie um novo conjunto e use add() para adicionar valores
+   - Crie um novo conjunto e use add() para adicionar variáveis
+*/
+
+// Exemplo
+// Criar uma set
+const letters = new set();
+
+// Adicionar valores a set
+letters.add('a');
+letters.add('b');
+letters.add('c');
+
+// Criar variáveis
+const a = 'a';
+const b = 'b';
+const c = 'c';
+
+// Adicionar variavéis a set
+letters.add(a);
+letters.add(b);
+letters.add(c);
