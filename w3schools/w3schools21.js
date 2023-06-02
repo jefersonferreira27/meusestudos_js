6a2ed194bad5a0a24881bfee08465ccf43fadb31
Jeferson Ferreira <105617299+jefersonferreira27@users.noreply.github.com>
2023-06-02 13:20:11 -0300

    Conhecendo o typeof

---
+++ w3schools/w3schools21.js
@@ -0,0 +1,33 @@
+// typeof em JS
+// Existem 5 tipos de dados diferentes que podem conter valores:
+- string
+- number
+- boolean
+- object
+- function
+
+// Existem 6 tipos de objetos
+- Object
+- Date
+- Array
+- String
+- Number
+- Boolean
+
+// E 2 tipos de dados que não podem conter valores:
+- null
+- undefined
+
+// O tipo de operador - typeof
+// Pode usar o typeof para localizar o tipo de dados de uma variável JS
+
+typeof "Jeferson" // Return string
+typeof 3.14 // Return number
+typeof NaN // Return number
+typeof false // Return boolean
+typeof [1,2,3] // Return object
+typeof {name:"Jeferson", idade:22} // Return object
+typeof new Date () // Return object
+typeof function () {} // Return function
+typeof myCar // Return undefined
+typeof null // Return object
